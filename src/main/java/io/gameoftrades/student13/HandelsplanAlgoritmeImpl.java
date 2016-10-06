9edd35812332c38077b1c0622df9c83b57f2996e
Fromalin <charlie.vierling@hotmail.com>
2016-10-06 12:28:36 +0200

    HandelsplanAlgortimeImpl

---
+++ src/main/java/io/gameoftrades/student13/HandelsplanAlgoritmeImpl.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.gameoftrades.student13;
+
+import io.gameoftrades.model.Wereld;
+import io.gameoftrades.model.algoritme.HandelsplanAlgoritme;
+import io.gameoftrades.model.markt.Handelsplan;
+import io.gameoftrades.model.markt.actie.HandelsPositie;
+
+/**
+ *
+ * @author Charlie
+ */
+public class HandelsplanAlgoritmeImpl implements HandelsplanAlgoritme{
+
+    @Override
+    public Handelsplan bereken(Wereld wereld, HandelsPositie hp) {
+        return null;
+    }
+    
+}
